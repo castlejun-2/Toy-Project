5d1101ae7cabb63f1a66441af55522ccd17c3c37
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-08-03 22:20:22 +0900

    버튼클릭 Handler 등록

--- RPSProject/client/src/components/views/LandingPage/GameList/GameList.js
+++ RPSProject/client/src/components/views/LandingPage/GameList/GameList.js
@@ -3,8 +3,28 @@ import Game from './Game';
 
 export default function GameList() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center space-y-6 bg-gray-100 px-4 sm:flex-row sm:space-x-6 sm:space-y-0">
-      <Game title="반응속도" subs="반응속도 게임입니다" />
+    <div className="flex flex-col items-center justify-center space-y-6 bg-gray-100 px-4 sm:flex-row sm:space-x-6 sm:space-y-0">
+      <Game
+        title="반응속도 게임"
+        subs="빠른 순발력을 요구하는 게임입니다."
+        src="https://toyrpsbucket.s3.ap-northeast-2.amazonaws.com/RPS_click.png"
+        start="/game/click"
+        description="/game/description/click"
+      />
+      <Game
+        title="사다리 타기"
+        subs="좋은 자리선정을 요구하는 게임입니다."
+        src="https://toyrpsbucket.s3.ap-northeast-2.amazonaws.com/RPS_ladder.png"
+        start="/game/ladder"
+        description="/game/description/ladder"
+      />
+      <Game
+        title="주사위 게임"
+        subs="높은 행운을 요구하는 게임입니다."
+        src="https://toyrpsbucket.s3.ap-northeast-2.amazonaws.com/RPS_dice.png"
+        start="/game/dice"
+        description="/game/description/dice"
+      />
     </div>
   );
 }
